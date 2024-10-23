fbcb99f3830222b09efabf0e1ef18498d807c83c
Zhuoang Tao <zhuoang2@illinois.edu>
2024-10-22 20:01:51 -0500

    seventh

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import DetailView from './components/DetailView';
 
 const App: React.FC = () => {
   return (
+    // <Routes basename = {process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<ListView />} />
       <Route path="/gallery" element={<GalleryView />} />
       <Route path="/pokemon/:name" element={<DetailView />} />
     </Routes>
+    // </Routes>
   );
 };
 
